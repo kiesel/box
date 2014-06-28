740713a7a7af466fcdb343f1739914c8c438f4a7
Alex Kiesel <alex@kiesel.name>
2014-06-28 08:53:01 +0200

    - add packages

---
+++ modules/system/manifests/packages.pp
@@ -0,0 +1,40 @@
+class system::packages {
+	$php-packages = [
+		'php5-cli',
+		'php5-curl',
+		'php5-fpm',
+		'php5-gd',
+		'php5-imap',
+		'php5-intl',
+		'php5-ldap',
+		'php5-json',
+		'php5-mcrypt',
+		'php5-mysql',
+		'php5-pgsql',
+		'php5-sqlite',
+		'php5-xdebug',
+		'php5-xsl'
+	]
+
+	package { $php-packages:
+		ensure => installed
+	}
+
+	$packages = [
+		'realpath',
+		'openjdk-7-jdk',
+		'openjdk-7-jre',
+		'git',
+		'git-svn',
+		'zsh',
+		'subversion',
+		'maven',
+		'tmux',
+		'screen',
+		'htop',
+	]
+
+	package { $packages:
+		ensure => installed
+	}
+}
