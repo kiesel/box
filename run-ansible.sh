66ec9b2effdc8d3cf09385d20ef0a1eb7622a3c6
Alex Kiesel <alex@kiesel.name>
2015-04-04 08:23:47 +0200

    Run ansible unbuffered, remove commented lines
    and bind exposed ports to ip 127.0.0.1

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
 set -e -u 
-
-echo "---> Running as $(whoami)"
 
 # Determine whether we need to install `ansible` itself
 which ansible >/dev/null || {
   echo "===> Installing ansible ..."
-  sudo aptitude update 
-  sudo aptitude install -y ansible 
+  sudo apt-get update
+  sudo apt-get install -y ansible
 }
 
 echo "===> Running ansible-playbook"
+
+# Copy hosts file into system and change mode to non-executable; once this file has the +x
+# bit set, ansible tries to interpret it by running it; given the host is a Windows system
+# you cannot reliably change the file's mode, therefore copy it into the machine
 sudo cp /vagrant/ansible/hosts /etc/ansible/hosts && sudo chmod 666 /etc/ansible/hosts
-sudo ansible-playbook --connection=local /vagrant/ansible/site.yml
+sudo PYTHONUNBUFFERED=1 ansible-playbook --connection=local /vagrant/ansible/site.yml
